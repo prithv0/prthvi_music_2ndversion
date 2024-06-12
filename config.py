cdee2a37e2d506c5acd48892dcb19509f5618861
prithv0 <bd946163@gmail.com>
2024-06-12 21:09:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/prashantsahlot/SYSTUM",
+    "https://github.com/prithv0/prthvi_music_2ndversion",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
